fb8b7b9e806d7e78f77a29039a3f84ef8efbfe07
alex <alex_kobilianskiy@bigmir.net>
2019-06-14 18:59:18 +0300

    s-intro complete

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,90 @@
 $(document).ready(function(){
 
-    $(".toggle-mnu").click(function() {
-        $(this).toggleClass("on");
-        return false;
+    // $(".toggle-mnu").click(function() {
+    //     $(this).toggleClass("on");
+    //     return false;
+    // });
+
+
+    /**
+     * mobile-mnu customization
+     */
+    var mmenu = $('#my-menu');
+    var $mmenu = mmenu.mmenu({
+        "pageScroll": true,
+        "navbar": {
+            "title" : "HAPPY&PREGNANT",
+        },
+        "extensions": [
+            // "position-front",
+            // "fx-listitems-slide",
+            "fullscreen",
+
+        ],
+    }, {
+        offCanvas: {
+            pageSelector: "#page-wrapper"
+        },
+        "autoHeight": true
     });
+
+    var mmenuBtn = $("#toggle-mnu");
+    var API = $mmenu.data("mmenu");
+
+    mmenuBtn.click(function() {
+        API.open();
+        $(this).addClass('on')
+    });
+
+    $('#mm-close-btn').click(function(e){
+        e.preventDefault();
+        API.close();
+    });
+
+    API.bind( "close:start", function() {
+        setTimeout(function() {
+            mmenuBtn.removeClass( "on" );
+        }, 300);
+    });
+    /**
+     * end mobile-mnu customization
+     */
+
+
+    $('img.svg').each(function(){
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        jQuery.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if(typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if(typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass+' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+        }, 'xml');
+    });
+
+
+
 
     //E-mail Ajax Send
     $("form").submit(function() { //Change
@@ -19,3 +100,4 @@ $(document).ready(function(){
         return false;
     });
 });
+
